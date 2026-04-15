5f00f7d402dc8a4f628a0764fd5c7929a186eab9
杨忠宇 <kane.yang@jaco.live>
2026-04-15 14:51:44 +0800

    fix
    Signed-off-by: 杨忠宇 <kane.yang@jaco.live>

--- mall-server/lib.go
+++ mall-server/lib.go
@@ -4,9 +4,9 @@ package main
 定义这个文件的目的是在执行 go mod tidy 时，保留在go.mod文件中的这些依赖
 */
 import (
-	_ "github.com/gin-gonic/gin"
+	_ "github.com/gin-gonic/gin" //web框架
 	_ "github.com/koding/multiconfig"
-	_ "github.com/sirupsen/logrus"
+	_ "github.com/sirupsen/logrus" //日志库
 	_ "github.com/urfave/cli/v2"
 	_ "gorm.io/driver/sqlite" // 需要保留的依赖
 	_ "gorm.io/gorm"          // 需要保留的依赖
