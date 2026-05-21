2cd7247bd617b7c582fe44319e11522d3fae6013
杨忠宇 <kane.yang@jaco.live>
2026-05-21 16:08:37 +0800

    feat: add category and region filter chips to home page

--- mall-mini/miniprogram/pages/home/home.ts
+++ mall-mini/miniprogram/pages/home/home.ts
@@ -1,6 +1,7 @@
 // pages/home/home.ts
 
 import { get } from '../../utils/request'
+import regionsData from '../../data/china-regions'
 
 interface ProductItem {
   id: number
@@ -19,6 +20,19 @@ interface HomeData {
   loading: boolean
   page: number
   hasMore: boolean
+  selectedCategory: string
+  selectedProvince: string
+  selectedCity: string
+  selectedDistrict: string
+  showCategoryPanel: boolean
+  showRegionPanel: boolean
+  regionStep: number
+  regionProvinceIndex: number
+  regionCityIndex: number
+  regionCities: string[]
+  regionDistricts: string[]
+  provinces: string[]
+  categories: string[]
 }
 
 Page<HomeData, WechatMiniprogram.IAnyObject>({
@@ -26,7 +40,20 @@ Page<HomeData, WechatMiniprogram.IAnyObject>({
     products: [],
     loading: false,
     page: 1,
-    hasMore: true
+    hasMore: true,
+    selectedCategory: '',
+    selectedProvince: '',
+    selectedCity: '',
+    selectedDistrict: '',
+    showCategoryPanel: false,
+    showRegionPanel: false,
+    regionStep: 0,
+    regionProvinceIndex: 0,
+    regionCityIndex: 0,
+    regionCities: [],
+    regionDistricts: [],
+    provinces: regionsData.map(p => p.name),
+    categories: ['电子产品', '服装鞋帽', '图书文具', '生活用品', '数码配件', '其他'],
   },
 
   onLoad() {
@@ -51,26 +78,30 @@ Page<HomeData, WechatMiniprogram.IAnyObject>({
     }
   },
 
-  // 加载商品列表
   async loadProducts() {
     if (this.data.loading) return
 
     this.setData({ loading: true })
 
     try {
+      const params: Record<string, string | number> = {
+        page: this.data.page,
+        page_size: 10,
+        status: 0,
+      }
+      if (this.data.selectedCategory) params.category = this.data.selectedCategory
+      if (this.data.selectedProvince) params.province = this.data.selectedProvince
+      if (this.data.selectedCity)     params.city     = this.data.selectedCity
+      if (this.data.selectedDistrict) params.district = this.data.selectedDistrict
+
       const response = await get<{ list: any[], total: number, page: number, page_size: number }>(
         '/api/product/search',
-        {
-          page: this.data.page,
-          page_size: 10,
-          status: 0 // 只获取在售商品
-        }
+        params
       )
 
       if (response.code === 0 && response.data) {
         const { list } = response.data
 
-        // 转换数据格式
         const products: ProductItem[] = list.map((item: any) => ({
           id: item.id,
           title: item.title,
@@ -99,7 +130,6 @@ Page<HomeData, WechatMiniprogram.IAnyObject>({
     }
   },
 
-  // 跳转到商品详情
   goToDetail(e: WechatMiniprogram.TouchEvent) {
     const { id } = e.currentTarget.dataset
     wx.navigateTo({
@@ -107,10 +137,78 @@ Page<HomeData, WechatMiniprogram.IAnyObject>({
     })
   },
 
-  // 搜索
   onSearch() {
     wx.navigateTo({
       url: '/pages/search/search'
     })
-  }
+  },
+
+  onOpenCategoryPanel() {
+    this.setData({ showCategoryPanel: true })
+  },
+
+  onCloseCategoryPanel() {
+    this.setData({ showCategoryPanel: false })
+  },
+
+  onSelectCategory(e: WechatMiniprogram.TouchEvent) {
+    const value: string = e.currentTarget.dataset.value
+    this.setData({ selectedCategory: value, showCategoryPanel: false, page: 1, hasMore: true, products: [] })
+    this.loadProducts()
+  },
+
+  onClearCategory(_e: WechatMiniprogram.TouchEvent) {
+    this.setData({ selectedCategory: '', page: 1, hasMore: true, products: [] })
+    this.loadProducts()
+  },
+
+  onOpenRegionPanel() {
+    this.setData({ showRegionPanel: true, regionStep: 0 })
+  },
+
+  onCloseRegionPanel() {
+    this.setData({ showRegionPanel: false })
+  },
+
+  onSelectProvince(e: WechatMiniprogram.TouchEvent) {
+    const pi: number = e.currentTarget.dataset.index
+    const province = regionsData[pi]
+    this.setData({
+      selectedProvince: province.name,
+      selectedCity: '',
+      selectedDistrict: '',
+      regionProvinceIndex: pi,
+      regionCities: province.children.map((c: any) => c.name),
+      regionStep: 1,
+    })
+  },
+
+  onSelectCity(e: WechatMiniprogram.TouchEvent) {
+    const ci: number = e.currentTarget.dataset.index
+    const pi = this.data.regionProvinceIndex
+    const city = regionsData[pi].children[ci]
+    this.setData({
+      selectedCity: city.name,
+      selectedDistrict: '',
+      regionCityIndex: ci,
+      regionDistricts: city.children as string[],
+      regionStep: 2,
+    })
+  },
+
+  onSelectDistrict(e: WechatMiniprogram.TouchEvent) {
+    const district: string = e.currentTarget.dataset.district
+    this.setData({ selectedDistrict: district, showRegionPanel: false, page: 1, hasMore: true, products: [] })
+    this.loadProducts()
+  },
+
+  onConfirmRegion() {
+    this.setData({ showRegionPanel: false, page: 1, hasMore: true, products: [] })
+    this.loadProducts()
+  },
+
+  onClearRegion(_e: WechatMiniprogram.TouchEvent) {
+    this.setData({ selectedProvince: '', selectedCity: '', selectedDistrict: '', page: 1, hasMore: true, products: [] })
+    this.loadProducts()
+  },
 })
