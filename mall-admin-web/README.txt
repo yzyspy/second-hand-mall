984abbaf0bdb84d5247d3bd024d097ec2f081b29
杨忠宇 <kane.yang@jaco.live>
2026-05-27 21:31:56 +0800

    fix
    Signed-off-by: 杨忠宇 <kane.yang@jaco.live>

---
+++ mall-admin-web/README.txt
@@ -0,0 +1,5 @@
+增加管理员账号：
+./mall-server-bin admin create-admin -c configs/config.yaml -u yzy -p root
+
+
+
--- mall-admin-web/components.d.ts
+++ mall-admin-web/components.d.ts
@@ -13,9 +13,12 @@ declare module 'vue' {
   export interface GlobalComponents {
     ElButton: typeof import('element-plus/es')['ElButton']
     ElCard: typeof import('element-plus/es')['ElCard']
+    ElDescriptions: typeof import('element-plus/es')['ElDescriptions']
+    ElDescriptionsItem: typeof import('element-plus/es')['ElDescriptionsItem']
     ElForm: typeof import('element-plus/es')['ElForm']
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
     ElIcon: typeof import('element-plus/es')['ElIcon']
+    ElImage: typeof import('element-plus/es')['ElImage']
     ElInput: typeof import('element-plus/es')['ElInput']
     ElMenu: typeof import('element-plus/es')['ElMenu']
     ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
