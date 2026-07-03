b915a916f519d0b946721dbfe9de1ebc90bf470e
杨忠宇 <kane.yang@jaco.live>
2026-07-03 12:46:14 +0800

    feat(ios): call AppSession.bootstrap() on app launch

--- mall-ios/MallApp.swift
+++ mall-ios/MallApp.swift
@@ -2,6 +2,10 @@ import SwiftUI
 
 @main
 struct MallApp: App {
+    init() {
+        AppSession.shared.bootstrap()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
